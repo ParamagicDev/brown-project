43568fdff902d65e3f7dd58370e84f31ac420bbf
paramagicdev <konnor5456@gmail.com>
2019-12-11 09:31:12 -0500

    add dom.js testing

--- __tests__/dom.test.js
+++ __tests__/dom.test.js
@@ -1,12 +1,17 @@
 import DOM from '../src/js/dom.js';
-// const DOM = require('../src/js/dom.js');
 
 describe('DOM()', () => {
   describe('createElement()', () => {
-    describe('Should create a div with class container', () => {
+    test('Should create a div with class container', () => {
       const element = DOM.createElement('div', {className: 'container'});
 
-      // expect(element.tagName).toBe('DIV');
+      expect(element.tagName).toBe('DIV');
+      expect(element.className).toBe('container');
+    });
+    test('Should create an unknown html element if given an improper tag', () => {
+      const element = DOM.createElement('improper-tag');
+
+      expect(element instanceof HTMLUnknownElement).toBe(true);
     });
   });
 });
